be81160130484959faa9132f0666ad4522a23b26
jatinsahijwani <117177197+jatinsahijwani@users.noreply.github.com>
2024-05-03 12:51:30 +0530

    medical history

---
+++ app/medical-history/page.tsx
@@ -0,0 +1,10 @@
+import { MedicalHistoryCard } from "@/components/medical-history-card";
+
+export default function Page()
+{
+    return (
+        <div className=" flex justify-center items-center w-[100vw] h-[100vh] bg-gradient-to-br from-gray-900 to-gray-400">
+        <MedicalHistoryCard />
+        </div>  
+    )
+}
