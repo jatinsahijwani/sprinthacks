71c5733b422ab44204b9a4442dc33f33e1ac8604
jatinsahijwani <117177197+jatinsahijwani@users.noreply.github.com>
2024-05-03 23:02:09 +0530

    chat added

---
+++ app/chatting/page.tsx
@@ -0,0 +1,12 @@
+import { Chatting } from '@/components/chatting'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Chatting />
+    </div>
+  )
+}
+
+export default page
